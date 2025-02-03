0a08426470191b99fb3cbf1f2ec3c2fd48c0202d
chdrchz <saavannaa.rose@gmail.com>
2025-02-03 17:50:11 -0600

    initial setup

---
+++ app/ui/topics/[id]/page.tsx
@@ -0,0 +1,11 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  /* await new Promise(resolve => setTimeout(resolve, 3000)); */
+
+  return <div>Topic Page: {id}</div>;
+}
